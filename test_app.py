6fba6363236356eb292dafef9bf6db7579e8b81c
sultanapursantosh <sultanapursantosh@gmail.com>
2026-09-04 08:40:40 +0530

    Modified test_app file to handle error

--- test_app.py
+++ test_app.py
@@ -1,5 +1,5 @@
 from app import app
 def test_home():
-    response=app.test_client.get("/")
-    assert response.status==200
+    response=app.test_client().get("/")
+    assert response.status_code==200
     assert response.data==b"hello world"
