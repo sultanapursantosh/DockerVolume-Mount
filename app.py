31a0a63df2d50ab600234b5ff36f7ed10f1eb1e1
sultanapursantosh <sultanapursantosh@gmail.com>
2026-09-06 01:37:14 +0530

    Modified app.py to run the app

--- app.py
+++ app.py
@@ -3,7 +3,9 @@ import logging
 import os
 
 app = Flask(__name__)
+
 os.makedirs("logs", exist_ok=True)
+
 logging.basicConfig(
     filename="logs/app.log",
     level=logging.DEBUG,
@@ -17,9 +19,15 @@ logger = logging.getLogger(__name__)
 def home():
 
     logger.debug("Entered home() function")
-
     logger.info("Home page requested")
-
     logger.warning("This is a warning example")
 
     return "hello world"
+
+
+if __name__ == "__main__":
+    app.run(
+        debug=True,
+        host="0.0.0.0",
+        port=5000
+    )
